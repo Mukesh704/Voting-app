d2d88247fde5d19cc7914273188f1c08f5ea156f
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-02-13 15:48:52 +0530

    login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -29,3 +29,31 @@ router.post('/signup', async(req, res) => {
         })
     }
 })
+
+router.post('/login', async(req, res) => {
+    try {
+        const {aadharCardNumber, password} = req.body;
+        const user = await User.findOne({aadharCardNumber: aadharCardNumber});
+
+        if(!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({
+                success: false,
+                error: 'Invalid Aadhar Number Or Password'
+            })
+        }
+
+        const payload = {
+            id: user.id
+        }
+        const token = generateToken(payload)
+        res.json({token})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            error: 'Internal Server Error'
+        })
+    }
+})
+
+module.exports = router;
